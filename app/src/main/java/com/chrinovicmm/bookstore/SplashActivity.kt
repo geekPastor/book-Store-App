da16eea0a9641981e66a922d90b358a1a5c9bacd
ChrinovicMM <chrinovicmukeba123@gmail.com>
2023-09-14 16:18:51 +0200

    feat{Authentification} finish dashboard

--- app/src/main/java/com/chrinovicmm/bookstore/SplashActivity.kt
+++ app/src/main/java/com/chrinovicmm/bookstore/SplashActivity.kt
@@ -4,15 +4,54 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        firebaseAuth = FirebaseAuth.getInstance()
         Handler().postDelayed(Runnable{
+            checkUserInfo()
+        }, 1000)
+    }
+
+    private fun checkUserInfo() {
+        var firebaseUser = firebaseAuth.currentUser
+
+        if (firebaseUser == null){
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 2000)
+        } else{
+            val ref = FirebaseDatabase.getInstance().getReference("Users")
+            ref.child(firebaseUser.uid)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(error: DatabaseError) {
+
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+
+                        //get user role User/Admin
+                        val userType = snapshot.child("").value
+                        if (userType == "user"){
+                            //simple user
+                            startActivity(Intent(this@SplashActivity, DashboardUserActivity::class.java))
+                            finish()
+                        } else if (userType == "admin"){
+                            //admin user
+                            startActivity(Intent(this@SplashActivity, DashboardAdminActivity::class.java))
+                            finish()
+                        }
+                    }
+                })
+        }
+
     }
 }
